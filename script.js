2226e1034448d34f7d72338dd8a0a4b2b0da9714
Mariusz Janowski <41803168+mariusz1802@users.noreply.github.com>
2023-02-22 12:53:24 +0100

    manio

---
+++ script.js
@@ -0,0 +1,19 @@
+class Todo{
+    constructor(){
+        this.todoArr = [];
+        this.todoList = todoList;
+    }
+    add(){
+        
+    }
+    delete(){}
+
+
+}
+
+const addBtn = document.querySelector("#addBtn");
+
+
+addBtn.addEventListener("click", ()=> {
+    console.log("add")
+})
